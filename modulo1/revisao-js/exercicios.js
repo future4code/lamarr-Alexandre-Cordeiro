eb2e94a565c9fccc34c36b1765d9f5fbb1125651
AlexandreRC <74365451+AlexandreRC@users.noreply.github.com>
2022-05-09 21:03:42 -0300

    Exercicio 3 terminado usando uma algoritimo que aprendi um tempo atrás

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -19,6 +19,24 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
+  const quickSort = (arr) => {
+    if(arr.length <= 1) return arr
+    const pivo = arr[0]
+    const esquerda = []
+    const direita = []
+
+    for (let i = 1; i < arr.length; i++) {
+        if(arr[i] < pivo) {
+            esquerda.push(arr[i])
+        }
+        else {
+            direita.push(arr[i])
+        }
+    }
+
+    return [...quickSort(esquerda), pivo, ...quickSort(direita)]
+  }
+  return quickSort(array)
 }
 
 // EXERCÍCIO 04
